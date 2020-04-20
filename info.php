83f48ed35ae86b54e162930edd97d4c0f446387f
Jaaziel234 <63311578+Jaaziel234@users.noreply.github.com>
2020-04-20 17:19:38 -0600

    Add files via upload

---
+++ info.php
@@ -0,0 +1,35 @@
+<?php
+//ARCHIVOS DE CONFIGURACION Y NAVBAR Y FOOTER
+include "global/config.php";
+include "global/db.php";
+include "carrito.php";
+include "templates/nabvar.php"
+?>
+
+ <html>
+                <head>
+                               
+                    <H1><strong>BIENVENDI@ A LA MISION Y VISION DE NUESTRA EMPRESA</H1></strong>
+
+                    <H1 align="justify"> <strong>MISION</H1></strong>
+                        
+                </head>
+                <body>
+                <br>
+                <h2><p ALIGN="justify">Satisfacer las necesidades de nuestros clientes y  superando sus expectativas,  brindándoles servicios de calidad. Aportando al desarrollo tecnológico y a la demanda  de productos innovadores con lo último en tecnología; ofrecer nuestro compromiso de excelencia e innovación. Dirigiéndonos a nuestros clientes como una empresa Salvadoreña. </p></h2>
+                
+
+                <H1 align="justify"><strong>VISION</H1></strong>
+
+           </head>
+                <body>
+                <br>
+               <h2><p ALIGN="justify"> Ser una empresa de productos tecnológicos,  como la mejor opción para la compra online de los mismos. Y a su vez la mejor opción para realizar su compra de un manera rápida o inmediata, ofreciendo seguridad en la compra de producto que el cliente necesite. </p></h2>
+
+                 <H2 align="center">GRACIAS POR PREFERIRNOS</H2>
+                  
+                </body>
+</html>
+<?php include "templates/footer.php"; ?>
+
+
