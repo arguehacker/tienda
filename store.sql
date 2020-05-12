1a8fc8ff660e620ec92a71fbb416925546f29db3
Jose-developer-start <60304928+Jose-developer-start@users.noreply.github.com>
2020-05-11 23:32:57 -0600

    Add files via upload

--- store.sql
+++ store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-05-2020 a las 18:01:10
+-- Tiempo de generación: 12-05-2020 a las 07:32:33
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.2.29
 
@@ -112,7 +112,8 @@ CREATE TABLE `detalleventa` (
 --
 
 INSERT INTO `detalleventa` (`ID`, `IDVENTA`, `IDPRODUCTO`, `PRECIOUNITARIO`, `CANTIDAD`, `VENDIDO`) VALUES
-(136, 62, 1, '3000.00', 1, 1);
+(136, 62, 1, '3000.00', 1, 1),
+(140, 64, 24, '1000.00', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -179,6 +180,7 @@ CREATE TABLE `ventas` (
   `Nombre` varchar(250) NOT NULL,
   `Fecha` datetime NOT NULL,
   `Correo` varchar(100) NOT NULL,
+  `Cantidad` int(100) NOT NULL,
   `Total` decimal(60,0) NOT NULL,
   `status` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -187,8 +189,9 @@ CREATE TABLE `ventas` (
 -- Volcado de datos para la tabla `ventas`
 --
 
-INSERT INTO `ventas` (`ID`, `ClaveTransacion`, `PaypalDatos`, `Nombre`, `Fecha`, `Correo`, `Total`, `status`) VALUES
-(62, 'hp9fd2pqq4utsl3t7g3v0e3s4j', '{\"id\":\"PAYID-L2PYEGY4YV74949XR699321T\",\"intent\":\"sale\",\"state\":\"approved\",\"cart\":\"7WP63784P04867338\",\"payer\":{\"payment_method\":\"paypal\",\"status\":\"VERIFIED\",\"payer_info\":{\"email\":\"sb-lq8xh959292@personal.example.com\",\"first_name\":\"John\",\"last_name\":\"Doe\",\"payer_id\":\"3FQ3UKUS2XJ6L\",\"shipping_address\":{\"recipient_name\":\"John Doe\",\"line1\":\"Free Trade Zone\",\"city\":\"San Salvador\",\"state\":\"San Salvador\",\"postal_code\":\"1120\",\"country_code\":\"SV\"},\"phone\":\"5034612073\",\"country_code\":\"SV\"}},\"transactions\":[{\"amount\":{\"total\":\"3000.00\",\"currency\":\"USD\",\"details\":{\"subtotal\":\"3000.00\",\"shipping\":\"0.00\",\"insurance\":\"0.00\",\"handling_fee\":\"0.00\",\"shipping_discount\":\"0.00\"}},\"payee\":{\"merchant_id\":\"628HUGKVDMKJ6\",\"email\":\"sb-ga3n61050275@business.example.com\"},\"description\":\"Compra de productos a Techmology box:$3,000.00\",\"custom\":\"hp9fd2pqq4utsl3t7g3v0e3s4j#62\",\"soft_descriptor\":\"PAYPAL *JOHNDOESTES\",\"item_list\":{\"shipping_address\":{\"recipient_name\":\"John Doe\",\"line1\":\"Free Trade Zone\",\"city\":\"San Salvador\",\"state\":\"San Salvador\",\"postal_code\":\"1120\",\"country_code\":\"SV\"}},\"related_resources\":[{\"sale\":{\"id\":\"3PW379681C364381M\",\"state\":\"completed\",\"amount\":{\"total\":\"3000.00\",\"currency\":\"USD\",\"details\":{\"subtotal\":\"3000.00\",\"shipping\":\"0.00\",\"insurance\":\"0.00\",\"handling_fee\":\"0.00\",\"shipping_discount\":\"0.00\"}},\"payment_mode\":\"INSTANT_TRANSFER\",\"protection_eligibility\":\"ELIGIBLE\",\"protection_eligibility_type\":\"ITEM_NOT_RECEIVED_ELIGIBLE,UNAUTHORIZED_PAYMENT_ELIGIBLE\",\"transaction_fee\":{\"value\":\"162.30\",\"currency\":\"USD\"},\"parent_payment\":\"PAYID-L2PYEGY4YV74949XR699321T\",\"create_time\":\"2020-04-21T23:32:11Z\",\"update_time\":\"2020-04-21T23:32:11Z\",\"links\":[{\"href\":\"https://api.sandbox.paypal.com/v1/payments/sale/3PW379681C364381M\",\"rel\":\"self\",\"method\":\"GET\"},{\"href\":\"https://api.sandbox.paypal.com/v1/payments/sale/3PW379681C364381M/refund\",\"rel\":\"refund\",\"method\":\"POST\"},{\"href\":\"https://api.sandbox.paypal.com/v1/payments/payment/PAYID-L2PYEGY4YV74949XR699321T\",\"rel\":\"parent_payment\",\"method\":\"GET\"}],\"soft_descriptor\":\"PAYPAL *JOHNDOESTES\"}}]}],\"create_time\":\"2020-04-21T23:30:35Z\",\"update_time\":\"2020-04-21T23:32:11Z\",\"links\":[{\"href\":\"https://api.sandbox.paypal.com/v1/payments/payment/PAYID-L2PYEGY4YV74949XR699321T\",\"rel\":\"self\",\"method\":\"GET\"}]}', 'jose rivas', '2020-04-22 01:28:40', 'sb-lq8xh959292@personal.example.com', '3000', 'Completo');
+INSERT INTO `ventas` (`ID`, `ClaveTransacion`, `PaypalDatos`, `Nombre`, `Fecha`, `Correo`, `Cantidad`, `Total`, `status`) VALUES
+(62, 'hp9fd2pqq4utsl3t7g3v0e3s4j', '{\"id\":\"PAYID-L2PYEGY4YV74949XR699321T\",\"intent\":\"sale\",\"state\":\"approved\",\"cart\":\"7WP63784P04867338\",\"payer\":{\"payment_method\":\"paypal\",\"status\":\"VERIFIED\",\"payer_info\":{\"email\":\"sb-lq8xh959292@personal.example.com\",\"first_name\":\"John\",\"last_name\":\"Doe\",\"payer_id\":\"3FQ3UKUS2XJ6L\",\"shipping_address\":{\"recipient_name\":\"John Doe\",\"line1\":\"Free Trade Zone\",\"city\":\"San Salvador\",\"state\":\"San Salvador\",\"postal_code\":\"1120\",\"country_code\":\"SV\"},\"phone\":\"5034612073\",\"country_code\":\"SV\"}},\"transactions\":[{\"amount\":{\"total\":\"3000.00\",\"currency\":\"USD\",\"details\":{\"subtotal\":\"3000.00\",\"shipping\":\"0.00\",\"insurance\":\"0.00\",\"handling_fee\":\"0.00\",\"shipping_discount\":\"0.00\"}},\"payee\":{\"merchant_id\":\"628HUGKVDMKJ6\",\"email\":\"sb-ga3n61050275@business.example.com\"},\"description\":\"Compra de productos a Techmology box:$3,000.00\",\"custom\":\"hp9fd2pqq4utsl3t7g3v0e3s4j#62\",\"soft_descriptor\":\"PAYPAL *JOHNDOESTES\",\"item_list\":{\"shipping_address\":{\"recipient_name\":\"John Doe\",\"line1\":\"Free Trade Zone\",\"city\":\"San Salvador\",\"state\":\"San Salvador\",\"postal_code\":\"1120\",\"country_code\":\"SV\"}},\"related_resources\":[{\"sale\":{\"id\":\"3PW379681C364381M\",\"state\":\"completed\",\"amount\":{\"total\":\"3000.00\",\"currency\":\"USD\",\"details\":{\"subtotal\":\"3000.00\",\"shipping\":\"0.00\",\"insurance\":\"0.00\",\"handling_fee\":\"0.00\",\"shipping_discount\":\"0.00\"}},\"payment_mode\":\"INSTANT_TRANSFER\",\"protection_eligibility\":\"ELIGIBLE\",\"protection_eligibility_type\":\"ITEM_NOT_RECEIVED_ELIGIBLE,UNAUTHORIZED_PAYMENT_ELIGIBLE\",\"transaction_fee\":{\"value\":\"162.30\",\"currency\":\"USD\"},\"parent_payment\":\"PAYID-L2PYEGY4YV74949XR699321T\",\"create_time\":\"2020-04-21T23:32:11Z\",\"update_time\":\"2020-04-21T23:32:11Z\",\"links\":[{\"href\":\"https://api.sandbox.paypal.com/v1/payments/sale/3PW379681C364381M\",\"rel\":\"self\",\"method\":\"GET\"},{\"href\":\"https://api.sandbox.paypal.com/v1/payments/sale/3PW379681C364381M/refund\",\"rel\":\"refund\",\"method\":\"POST\"},{\"href\":\"https://api.sandbox.paypal.com/v1/payments/payment/PAYID-L2PYEGY4YV74949XR699321T\",\"rel\":\"parent_payment\",\"method\":\"GET\"}],\"soft_descriptor\":\"PAYPAL *JOHNDOESTES\"}}]}],\"create_time\":\"2020-04-21T23:30:35Z\",\"update_time\":\"2020-04-21T23:32:11Z\",\"links\":[{\"href\":\"https://api.sandbox.paypal.com/v1/payments/payment/PAYID-L2PYEGY4YV74949XR699321T\",\"rel\":\"self\",\"method\":\"GET\"}]}', 'jose rivas', '2020-04-22 01:28:40', 'sb-lq8xh959292@personal.example.com', 0, '3000', 'Completo'),
+(64, 'kca0pgah0edfhak6t6mskb3lld', '', 'José Miguel', '2020-05-12 07:20:18', 'jose@service', 4, '4000', 'pendiente');
 
 --
 -- Índices para tablas volcadas
@@ -252,7 +255,7 @@ ALTER TABLE `administrador`
 -- AUTO_INCREMENT de la tabla `detalleventa`
 --
 ALTER TABLE `detalleventa`
-  MODIFY `ID` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=140;
+  MODIFY `ID` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=141;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
@@ -264,7 +267,7 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- Restricciones para tablas volcadas
